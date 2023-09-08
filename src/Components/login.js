3616f534893b2667c329d13f4a599c7c8a36fe65
Ale77$ <encuadernacionyali@gmail.com>
2023-09-08 10:23:34 -0600

    Tiene algunos cambios en CSS

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,27 +1,33 @@
 import { onNavigate  } from "../main.js";
 import { entrarConGoogle } from "../FirebaseFn.js";
 
-
 //const auth = getAuth();
-
 export const login = () => {
     const div = document.createElement('div'); // se crea para insertar el boton
     div.classList.add('screen-login')
-
-    const section = document.createElement('section')
-    section.classList.add('section-login')
 
     const title = document.createElement('h1');
     title.textContent = 'HerCode';
     title.classList.add('HerCode');
 
+    const section = document.createElement('section')
+    section.classList.add('section-login')
+//INPUT PARA INGRESAR CORREO
     const inputEmail = document.createElement('input');
-    const inputPass = document.createElement('input')
-    const startButton= document.createElement('button'); 
+    inputEmail.textContent = "Ingresa tu correo electrónico";
+    inputEmail.classList.add('input-email');
+//INPUT PARA INGRESAR CONTRASEÑA
+    const inputPass = document.createElement('input');
+    inputPass.textContent = "Ingresa tu contraseña";
+    inputPass.classList.add = ('input-pass');
+//BOTON INGRESAR
+    const loginButton= document.createElement('button'); 
+    loginButton.textContent = 'Ingresar'; 
+    loginButton.classList.add = ('btn-login')
+//BOTON INGRESAR CON GOOGLE
     const googleBotton = document.createElement('button');
     googleBotton.textContent= 'Entrar con Google';
     googleBotton.addEventListener("click", ()=>{
-
         entrarConGoogle()
         .then((user)=>{
             onNavigate('/programmingWall')
@@ -29,8 +35,6 @@ export const login = () => {
             alert('revisa tus datos')
         })
     })
-    startButton.textContent = 'Ingresar'; //Agregar texto a boton
-    
-    div.append(title,inputEmail,inputPass,startButton,googleBotton);// append()uctiliza para agregar uno o varios elementos 
+    div.append(title,inputEmail,inputPass,loginButton,googleBotton);// append()uctiliza para agregar uno o varios elementos 
     return div;
     };
--- src/Components/start.js
+++ src/Components/start.js
@@ -1,15 +1,14 @@
 import { onNavigate  } from "../main.js";
 
 export const start = () => {
-    const div =document.createElement('div'); // se crea para insertar el boton
+    const div =document.createElement('div');
     div.classList.add('screen-start')
     const title = document.createElement('h1');
+    title.textContent = 'HerCode';
     title.classList.add('HerCode');
+    //BOTON START
     const startButton= document.createElement('button');
-    startButton.classList.add('btn-start');
-
-    title.textContent = 'HerCode'; 
-    
+    startButton.classList.add('btn-start'); 
     startButton.textContent = 'Inicio'; //Agregar texto a boton
     
     startButton.addEventListener('click', ()=> {
