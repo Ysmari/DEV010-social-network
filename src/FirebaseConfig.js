4b165d6b765056b1ab04edaffcb1321131aaea81
Ale77$ <encuadernacionyali@gmail.com>
2023-09-13 13:19:44 -0600

    Tiene un nuevo test y se crearon carpetas de eslintrc y de mocks

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -2,18 +2,18 @@
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBcT7hblI4Z5vQ4PDEsEkrjuRIyAoCOImA",
-  authDomain: "social-network-hercode.firebaseapp.com",
-  projectId: "social-network-hercode",
-  storageBucket: "social-network-hercode.appspot.com",
-  messagingSenderId: "134195357770",
-  appId: "1:134195357770:web:93bc295e8beebdca835eb3"
-};
+  apiKey: 'AIzaSyBcT7hblI4Z5vQ4PDEsEkrjuRIyAoCOImA',
+  authDomain: 'social-network-hercode.firebaseapp.com',
+  projectId: 'social-network-hercode',
+  storageBucket: 'social-network-hercode.appspot.com',
+  messagingSenderId: '134195357770',
+  appId: '1:134195357770:web:93bc295e8beebdca835eb3'
+}
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-export{auth}
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+export { auth }
