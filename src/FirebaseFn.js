8bed436276998432e171e7bf50e8a39dafba96f7
Ale77$ <encuadernacionyali@gmail.com>
2023-09-14 17:05:22 -0600

    Cambios en funcion para crear ususario-NO FUNCIONALES

--- src/FirebaseFn.js
+++ src/FirebaseFn.js
@@ -1,8 +1,23 @@
-/* eslint-disable no-unused-vars */
-import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth' // FUNCION AUTENTICAR CON GOOGLE
+import { createUserWithEmailAndPassword } from 'firebase/auth' // FUNCION CREAR USUARIO
 import { auth } from './FirebaseConfig.js'
+// FUNCION AUTENTICAR CON GOOGLE
 const provider = new GoogleAuthProvider()
-
 export const entrarConGoogle = () => {
   return signInWithPopup(auth, provider)
+}
+// FUNCION CREAR USUSARIO
+// const auth = getAuth(app)
+function registrarUsuario(email, password){
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code
+    const errorMessage = error.message
+    // ..
+  })
 }
