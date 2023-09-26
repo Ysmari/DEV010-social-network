d39352ee311e355ea681f9e849aac31a09ec135b
DianaJ-Dev <134237911+DianaJ-Dev@users.noreply.github.com>
2023-09-26 12:08:34 -0500

    envio informacion de post a firebase pero no se muestra en la app

--- src/FirebaseFn.js
+++ src/FirebaseFn.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
-import { signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, fetchSignInMethodsForEmail } from 'firebase/auth'
-import { auth } from './FirebaseConfig.js'
+import { signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { collection, addDoc } from 'firebase/firestore'
+import { auth, db } from './FirebaseConfig.js'
+
 // FUNCION AUTENTICAR CON GOOGLE
 const provider = new GoogleAuthProvider()
 
@@ -14,4 +16,8 @@ export const registrarConCorreoContrasena = (email, password) => {
 // FUNCION PARA ACCEDER A UNA CUENTA EXISTENTE CON EMAIL Y PASSWORD
 export const UsuarioConSesionActiva = (email, password) => {
   return signInWithEmailAndPassword(auth, email, password)
+}
+// FUNCION PARA CREAR POST
+export const createPostProgrammingWall = (obj) => {
+  return addDoc(collection(db, 'posts'), obj)
 }
