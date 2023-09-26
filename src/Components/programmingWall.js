091b6b2e4df4e1ee7e737865bf89be0e9d3ca407
DianaJ-Dev <134237911+DianaJ-Dev@users.noreply.github.com>
2023-09-26 18:44:51 -0500

    Cambios no funcionales

--- src/Components/programmingWall.js
+++ src/Components/programmingWall.js
@@ -6,10 +6,16 @@ function programmingWall (navigateTo) {
   const title = document.createElement('h1')
   title.textContent = 'ventana de programacion'
 
+  const postForm = document.createElement('form')
+  postForm.classList.add('postForm')
+
   const inputIngresopost = document.createElement('input')
   inputIngresopost.placeholder = 'Ingresa tu post'
   inputIngresopost.classList.add('inputPost')
   inputIngresopost.id = 'inputPost'
+
+  const contenedorPost = document.createElement('div')
+  contenedorPost.textContent = inputIngresopost.value
 
   const buttonReturn = document.createElement('button')
   buttonReturn.textContent = 'cerrar'
@@ -18,7 +24,9 @@ function programmingWall (navigateTo) {
   })
 
   const buttonCrear = document.createElement('button')
+  buttonCrear.type = 'submit'
   buttonCrear.textContent = 'Crear'
+
   buttonCrear.addEventListener('click', () => {
     console.log('text', inputIngresopost.value)
     const newPost = {
@@ -28,13 +36,33 @@ function programmingWall (navigateTo) {
     createPostProgrammingWall(newPost)
       .then((docRef) => {
         console.log('Documento agregado con ID: ', docRef.id)
+        contenedorPost.appendChild(createPostElement(newPost))
       })
       .catch((error) => {
         console.error('Error al agregar el documento: ', error)
       })
   })
 
-  section.append(title, buttonReturn, inputIngresopost, buttonCrear) // append agrega nuevo elemento al contenedor en este caso agrega tittle a section que es el principal
+  function createPostElement (post) {
+    const postElement = document.createElement('div')
+    postElement.classList.add('post') // Agrega una clase CSS para dar estilo
+
+    const postContent = document.createElement('p')
+    postContent.textContent = post.text // Puedes personalizar esto según tu estructura de datos
+
+    const postDate = document.createElement('p')
+    postDate.textContent = post.date.toDateString() // Personaliza la presentación de la fecha
+
+    // Agrega estos elementos al postElement
+    postElement.appendChild(postContent)
+    postElement.appendChild(postDate)
+
+    return postElement
+  }
+
+  // Consulta Firestore para obtener los posts y mostrarlos en tu muro.
+
+  section.append(title, buttonReturn, inputIngresopost, buttonCrear, postForm) // append agrega nuevo elemento al contenedor en este caso agrega tittle a section que es el principal
   return section
 }
 
