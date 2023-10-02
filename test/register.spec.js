9de07a251402e541f3d34914a8f5cf508e6f3b69
DianaJ-Dev <134237911+DianaJ-Dev@users.noreply.github.com>
2023-10-02 18:14:36 -0500

    Contiene test de register completo

--- test/register.spec.js
+++ test/register.spec.js
@@ -16,4 +16,18 @@ describe('botonRegistro', () => {
     component.querySelector('.btn-register2').click()
     expect(registrarConCorreoContrasena).toHaveBeenCalled()
   })
+  it('se debe indicar cuando el correo ya esta en uso', () => {
+    registrarConCorreoContrasena.mockRejectedValue({ code: 'auth/invalid-email' })
+    const alertSpy = jest.spyOn(window, 'alert') // Espía la función de alerta
+    const component = register()
+    component.querySelector('.btn-register2').click()
+    expect(alertSpy).toHaveBeenCalledWith('Ingresar un Correo Valido')
+  })
+  it('se debe indicar que la contraseña debe tener maximo 7 caracteres', () => {
+    registrarConCorreoContrasena.mockRejectedValue({ code: 'auth/email-already-in-use' })
+    const alertSpy = jest.spyOn(window, 'alert') // Espía la función de alerta
+    const component = register()
+    component.querySelector('.btn-register2').click()
+    expect(alertSpy).toHaveBeenCalledWith('La contraseña debe tener mínimo 7 caracteres')
+  })
 })
