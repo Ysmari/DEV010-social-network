e32b6a4744f9bb27c5e084ffcaacbbb5dbd7d4d5
Ale77$ <encuadernacionyali@gmail.com>
2023-10-17 10:15:45 -0600

    Intento 3 para deploy

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig(({ command }) => {
   if (command === 'build') {
     return {
       root: 'src',
-      base: '/<REPO>/',
       // para hacer un deploy en github pages, configura propiedad base con el
       // nombre/url de tu repo
       // para fazer uma implantação github pages, defina a propriedade base
